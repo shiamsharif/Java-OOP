c2759c12893d872e3d3138b9d0c3610affff4cf0
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-30 20:56:43 +0600

    ADD: Object

---
+++ Object/Car.java
@@ -0,0 +1,21 @@
+package Object;
+
+public class Car {
+    
+    String make = "BMW";
+    String model = "X7";
+    int year = 2023;
+    String color = "Black";
+    double price = 50000.00;
+
+    void drive(){
+        System.err.println("You drive the car.");
+    }
+    
+    void brake(){
+        System.err.println("You stop on the brakes.");
+    }
+}
+
+    
+
---
+++ Object/Main.java
@@ -0,0 +1,24 @@
+package Object;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Car Mycar1 = new Car();
+        Car Mycar2 = new Car();
+
+        System.err.println(Mycar1.model);
+        System.err.println(Mycar1.make);
+        System.err.println();
+        System.err.println(Mycar2.model);
+        System.err.println(Mycar2.make);
+        
+    }
+}
+
+// Output:
+
+// X7
+// BMW
+
+// X7
+// BMW
